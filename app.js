407c28392f7eb9e3567907a809111cd0c32d292f
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-08 10:08:31 +0700

    up

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-const List ={
-    render(){
-
+const http = require('http');
+ 
+const server = http.createServer((req, res) =>{
+    const url = req.url;
+    if(url == '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+          res.write('<body>')
+           res.write('<h1>Hello word! </h1>');
+           res.write('<ul><li>User 1</li></ul>')
+         res.write('</body>')
+        res.write('</html>')
+       res.end(JSON.stringify(data));
+    
+    }else{
+        res.write('<html>')
+           res.write('<body>')
+             res.write('<h1> Home ppage</h1>');
+          res.write('</body>')
+        res.write('</html>')
+        res.end();
     }
-}
-export default List;
+});
+
+
+const PORT = 3000;
+
+server.listen(PORT, () =>{
+    console.log(`Server running port ${PORT}`);
+})
