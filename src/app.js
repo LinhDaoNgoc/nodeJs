6a32773dbbc13092ba5e816c6ed09d97c38014c5
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-09 08:49:59 +0700

    comit new

--- src/app.js
+++ src/app.js
@@ -1,39 +1,15 @@
-const express = require('express');
+const express = require("express");
+const productRouter = require('./routes/product');
+
 const app = express();
+// middleware
+app.use(express.json());
 
-//routing 
-app.get('/', (req, res) => {
-  res.send("<h1>Home Page</h1>");
-});
-app.get('/api/products', (req, res) => {
-  const products = [{ id: 1, name: "Product 1" }, { id: 2, name: "Product 2" }]
-  res.json(products);
-});
+// Routing
+app.use("/api", productRouter);
 
-//connect 
+// Connect
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running port ${PORT}`);
-})
-
-
-// const server = http.createServer((req, res) =>{
-//     const url = req.url;
-//     if(url == '/'){
-//         res.setHeader('Content-Type', 'text/html');
-//         res.write('<html>')
-//           res.write('<body>')
-//            res.write('<h1>Hello word! </h1>');
-//            res.write('<ul><li>User 1</li></ul>')
-//          res.write('</body>')
-//         res.write('</html>')
-//        res.end(JSON.stringify(data));
-//     }else{
-//         res.write('<html>')
-//            res.write('<body>')
-//              res.write('<h1> Home ppage</h1>');
-//           res.write('</body>')
-//         res.write('</html>')
-//         res.end();
-//     }
-// });
+  console.log(`Server đang chạy cổng ${PORT}`);
+});
