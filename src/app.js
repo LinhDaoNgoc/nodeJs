a49cb9ceee03889492a2edd2fd7e34fd9f8979df
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-09 07:59:45 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,30 +1,39 @@
-const http = require('http');
- 
-const server = http.createServer((req, res) =>{
-    const url = req.url;
-    if(url == '/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>')
-          res.write('<body>')
-           res.write('<h1>Hello word! </h1>');
-           res.write('<ul><li>User 1</li></ul>')
-         res.write('</body>')
-        res.write('</html>')
-       res.end(JSON.stringify(data));
-    
-    }else{
-        res.write('<html>')
-           res.write('<body>')
-             res.write('<h1> Home ppage</h1>');
-          res.write('</body>')
-        res.write('</html>')
-        res.end();
-    }
+const express = require('express');
+const app = express();
+
+//routing 
+app.get('/', (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
+app.get('/api/products', (req, res) => {
+  const products = [{ id: 1, name: "Product 1" }, { id: 2, name: "Product 2" }]
+  res.json(products);
 });
 
-
-const PORT = 3000;
-
-server.listen(PORT, () =>{
-    console.log(`Server running port ${PORT}`);
+//connect 
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running port ${PORT}`);
 })
+
+
+// const server = http.createServer((req, res) =>{
+//     const url = req.url;
+//     if(url == '/'){
+//         res.setHeader('Content-Type', 'text/html');
+//         res.write('<html>')
+//           res.write('<body>')
+//            res.write('<h1>Hello word! </h1>');
+//            res.write('<ul><li>User 1</li></ul>')
+//          res.write('</body>')
+//         res.write('</html>')
+//        res.end(JSON.stringify(data));
+//     }else{
+//         res.write('<html>')
+//            res.write('<body>')
+//              res.write('<h1> Home ppage</h1>');
+//           res.write('</body>')
+//         res.write('</html>')
+//         res.end();
+//     }
+// });
