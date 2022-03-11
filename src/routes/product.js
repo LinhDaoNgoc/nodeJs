e07da736001211b947e73778be727f4aab2357ff
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-11 08:05:18 +0700

    upadate buoi 3

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,12 +3,15 @@ const { checkAuth } = require('../middleware/checkAuth');
 
 const router = Router();
 
-router.get("/products", checkAuth, (req, res) => {
+//fake data
+const products = [{ id: 1, name: "Product A", }, { id: 2, name: "Product B", }];
+
+router.get("/products", checkAuth, (req, res) => { //get all
     const products = [{ id: 1, name: "Product A", }, { id: 2, name: "Product B", },];
     res.json(products);
 });
 
-router.post('/products', checkAuth, (req, res) => {
+router.post('/products', checkAuth, (req, res) => { //creat product
     const products = [{ id: 1, name: "Product A", }, { id: 2, name: "Product B", },];
     products.push(req.body);
     res.json(products);
